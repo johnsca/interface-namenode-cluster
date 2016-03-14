a055726b5e09cfe8ef56811211bcc61cd6748442
Andrew McLeod <admcleod@gmail.com>
2016-03-14 15:59:01 +0100

    send ssh flattened to one function

--- peers.py
+++ peers.py
@@ -21,7 +21,7 @@ from jujubigdata import utils
 
 class NameNodePeers(RelationBase):
     scope = scopes.UNIT
-    auto_accessors = ['ssh-key-active', 'ssh-key-standby']
+    auto_accessors = ['ssh-key', 'ssh-key']
 
     @hook('{peers:namenode-cluster}-relation-joined')
     def joined(self):
@@ -66,13 +66,9 @@ class NameNodePeers(RelationBase):
                     hookenv.leader_set(validated_namenodes=checked_nodes)
                     return checked_nodes
     
-    def send_ssh_key_active(self, ssh_key):
+    def send_ssh(self, ssh_key):
         for conv in self.conversations():
-            conv.set_remote('ssh-key-active', ssh_key)
-
-    def send_ssh_key_standby(self, ssh_key):
-        for conv in self.conversations():
-            conv.set_remote('ssh-key-standby', ssh_key)
+            conv.set_remote('ssh-key', ssh_key)
 
     def hosts_map(self):
         result = {}
